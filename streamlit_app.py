c90904553c6d466bd09327a538e99b4e9bd78a2f
Wasiak-Remigiusz-Study <wasiak.r.study@gmail.com>
2025-09-29 15:56:06 +0200

    Update streamlit_app.py
    deleted pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Convert Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
