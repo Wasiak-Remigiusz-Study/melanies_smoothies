9090128203603b50b89e4a61afa2ae4168317e7d
Wasiak-Remigiusz-Study <wasiak.r.study@gmail.com>
2025-09-29 15:53:58 +0200

    Update streamlit_app.py
    added second df

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 
 # Write directly to the app
-st.title(":cup_with_straw:Customize Your Smoothie!:cup_with_straw:")
+st.title("Customize Your Smoothie!:cup_with_straw:")
 st.write(
   """Choose the fruits you want in your Smoothie!
   """
@@ -24,6 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.dataframe(pd_df)
 st.stop()
 
